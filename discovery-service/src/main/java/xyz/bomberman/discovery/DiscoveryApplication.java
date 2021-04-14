9ddebf66c4e7226ce595bc57a5254af4f9aaa1ec
Sergey Tselovalnikov <sergeicelov@gmail.com>
2021-04-15 09:40:10 +1000

    listen to the port

--- discovery-service/src/main/java/xyz/bomberman/discovery/DiscoveryApplication.java
+++ discovery-service/src/main/java/xyz/bomberman/discovery/DiscoveryApplication.java
@@ -4,15 +4,18 @@ import io.rsocket.core.RSocketServer;
 import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.netty.server.TcpServerTransport;
 
+import java.util.Objects;
+
 public class DiscoveryApplication {
 
   public static void main(String[] args) {
     final ServiceRegistry serviceRegistry = new ServiceRegistry();
 
+    var port = Objects.requireNonNull(System.getenv("PORT"), "8081");
     var server = RSocketServer.create()
         .payloadDecoder(PayloadDecoder.ZERO_COPY)
         .acceptor(new DiscoverySocketAcceptor(serviceRegistry))
-        .bindNow(TcpServerTransport.create(8081));
+        .bindNow(TcpServerTransport.create(Integer.parseInt(port)));
 
 
     System.out.println("started on " + server.address());
